4c735761a4c60c57cb2f2de72e685bd85877d087
Vera Lykke Kaasen <vera.lykke.kaasen@gmail.com>
2022-03-20 22:07:57 +0100

    adding images and icons

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,26 @@
-import react from "react";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  UserIcon,
+} from "@heroicons/react";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <Header className="">
-      <h1>Sebastian</h1>
-    </Header>
+    <header className="">
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        alt="hulu"
+        width={200}
+        height={100}
+      />
+    </header>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -10,7 +10,6 @@ export default function Home() {
         <title>Hulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Header />
     </div>
   );
