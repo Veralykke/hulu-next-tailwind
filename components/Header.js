6910d158decaa32a602b5387287f7a3453c7cf79
Vera Lykke Kaasen <vera.lykke.kaasen@gmail.com>
2022-03-20 13:46:10 +0100

    adding css

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Header() {
+  return (
+    <Header className="">
+      <h1>Sebastian</h1>
+    </Header>
+  );
+}
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+    </div>
+  );
+}
