b5132803c2158c9123d25bf779debbea939d31db
Vera Lykke Kaasen <vera.lykke.kaasen@gmail.com>
2022-03-21 18:38:24 +0100

    Adding movie results

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <Head>
@@ -14,6 +16,23 @@ export default function Home() {
 
       <Header />
       <Nav />
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.Results,
+    },
+  };
 }
